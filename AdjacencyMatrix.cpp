3363d5523a252a55330d56212b90602a852da600
samgallic <93098826+samgallic@users.noreply.github.com>
2022-11-28 10:35:37 -0500

    Add files via upload

--- AdjacencyMatrix.cpp
+++ AdjacencyMatrix.cpp
@@ -2,8 +2,38 @@
 
 AdjacencyMatrix::AdjacencyMatrix() {
     indexCounter = 0;
+
+    for(int i = 0; i < 10000; i++) {
+        for(int j = 0; j < 10000; j++) {
+            matrix[i][j] = 0;
+        }
+    }
 }
 
 void AdjacencyMatrix::InsertEdge(int firstISBN, int secondISBN) {
+    if(indexMatcher.count(firstISBN) == 0) {
+        indexMatcher.emplace(firstISBN, indexCounter);
+        reverseIndexMatcher.emplace(indexCounter, firstISBN);
+        indexCounter++;
+    }
+    if(indexMatcher.count(secondISBN) == 0) {
+        indexMatcher.emplace(secondISBN, indexCounter);
+        reverseIndexMatcher.emplace(indexCounter, secondISBN);
+        indexCounter++;
+    }
 
+    matrix[indexMatcher[firstISBN]][indexMatcher[secondISBN]]++;
+    matrix[indexMatcher[secondISBN]][indexMatcher[firstISBN]]++;
+}
+
+int AdjacencyMatrix::GetWeight(int firstISBN, int secondISBN) {
+    return matrix[indexMatcher[firstISBN]][indexMatcher[secondISBN]];
+}
+
+void AdjacencyMatrix::GetAdjacentNodes(int isbn, unordered_map<int, int>& allNodes) {
+    for(int i = 0; i < 10000; i++) {
+        if(matrix[indexMatcher[isbn]][i] != 0) {
+            allNodes.emplace(reverseIndexMatcher[i], matrix[indexMatcher[isbn]][i]);
+        }
+    }
 }
--- AdjacencyMatrix.h
+++ AdjacencyMatrix.h
@@ -5,8 +5,11 @@ using namespace std;
 class AdjacencyMatrix {
     int matrix[10000][10000];
     unordered_map<int, int> indexMatcher;
+    unordered_map<int, int> reverseIndexMatcher;
     int indexCounter;
 public:
     AdjacencyMatrix();
     void InsertEdge(int firstISBN, int secondISBN);
+    int GetWeight(int firstISBN, int secondISBN);
+    void GetAdjacentNodes(int isbn, unordered_map<int, int>& allNodes);
 };
