de5af6f79769046740143f47ea76b546d01da5df
wang-angela <angela.wang@ufl.edu>
2022-12-04 16:24:27 -0500

    GUI Update and Modified

--- main.cpp
+++ main.cpp
@@ -33,6 +33,54 @@ int main() {
     // Create matrix as each value becomes available
     readReviews(reviews, bookMapper, matrix, "../data/ratings_trunicated.csv");
 
+    cout << "Welcome to Book Recommender!" << endl;
+    cout << "First, enter ISBN of your favorite books." << endl;
+    cout << "If you are done, enter \"done\"." << endl;
+
+// Reads in ISBNs.
+    string input;
+    int inputNum;
+    vector<long> isbns;
+
+    while(input.compare("done") != 0) {
+        bool isADigit = true;
+        cin >> input;
+        for(int k = 0; k < input.size(); k++) {
+            if(!isdigit(input.at(k))) {
+                isADigit = false;
+            }
+        }
+
+        if(isADigit) {
+            inputNum = stol(input);
+            isbns.push_back(inputNum);
+        }
+    }
+
+// When done is entered, asks user for how many books they would like to see.
+    cout << endl << "Thank you!" << endl;
+    cout << "Now, tell us how many books you would like us to recommend." << endl;
+
+    int numOfBooks;
+    cin >> numOfBooks;
+
+    cout << endl << "Thanks! Picking books for you ..." << endl;
+
+//Calculate the whole thing with matrix and heap
+
+    cout << "Recommended books are:" << endl;
+
+//display the result
+
+    cout << endl << "Thank you for using Fish Against Education Book Recommender!" << endl;
+
+    for (int j = 0; j < 2; ++j)
+    {
+        for (int i = 0; i < 70; ++i)
+            cout << "*";
+        cout << endl;
+    }
+
     //contains bookID of user's favorite books
     //edit this to see different result
     //currently has Hunger Game (ID: 1) and Harry Potter (ID: 2)
@@ -43,6 +91,7 @@ int main() {
     //currently recommends 3 books
     //heap.recommendBooks(3);
 
+    delete matrix;
     return 0;
 }
 
