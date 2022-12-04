ddb9ee580d25b8a8509399d1f3eb0ac753423581
wang-angela <92885742+wang-angela@users.noreply.github.com>
2022-12-04 15:35:14 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@ void readBooks(std::unordered_map<long, Book>& books,
                std::unordered_map<int, long>& bookMapper, const std::string& file);
 
 void readReviews(std::unordered_map<int, std::vector<int>>& reviews,
+                 std::unordered_map<int, long>& bookMapper, AdjacencyMatrix* matrix,
                  const std::string& file);
 
 int main() {
@@ -23,51 +24,19 @@ int main() {
     std::unordered_map<long, Book> books;
     readBooks(books, bookMapper, "../data/books.csv");
 
-    // Key: User id; Value: Vector of Liked Books (Books rated 4 and up)
-    std::unordered_map<int, std::vector<int>> reviews;
-    readReviews(reviews, "../data/ratings.csv");
-
     // Set up Adjacency Matrix
     AdjacencyMatrix* matrix = new AdjacencyMatrix();
-    for (auto& review : reviews) {
-        for (int i = 0; i < review.second.size(); i++) {
-            for (int j = i + 1; j < review.second.size(); j++) {
-                matrix->InsertEdge(bookMapper[review.second.at(i)],
-                                   bookMapper[review.second.at(j)]);
-            }
-        }
-    }
-    //matrix->PrintMatrix();
 
-    /*
-    int numOfBooks = 0;
-    std::cout << "Number of books to enter:" << std::endl;
-    std::cin >> numOfBooks;
+    // Key: User id; Value: Vector of Liked Books (Books rated 4 and up)
+    std::unordered_map<int, std::vector<int>> reviews;
 
-    long isbn;
-    std::vector<int> userInput;
-    std::cout << "Please enter the isbn of your favorite books:" << std::endl;
-    for (int i = 0; i <numOfBooks; i++) {
-
-        std::cin >> isbn;
-        // If isbn exists and is valid
-        if (books.count(isbn) == 1) {
-            userInput.push_back(isbn);
-            std::cout << "ISBN FOUND!" << std::endl;
-        }
-        else {
-            std::cout << "ISBN NOT FOUND!" << std::endl;
-        }
-    }
-
-
-    */
-
+    // Create matrix as each value becomes available
+    readReviews(reviews, bookMapper, matrix, "../data/ratings_trunicated.csv");
 
     //contains bookID of user's favorite books
     //edit this to see different result
     //currently has Hunger Game (ID: 1) and Harry Potter (ID: 2)
-    //std::vector<int> userInput = {439023483, 2};
+    //std::vector<int> userInput = {1, 2};
     //MinHeap heap(userInput, reviews);
 
     //Just need to edit this function to display book title instead of ID's
@@ -171,7 +140,8 @@ void readBooks(std::unordered_map<long, Book>& books,
 }
 
 void readReviews(std::unordered_map<int, std::vector<int>>& reviews,
-                 const std::string& file) {
+                 std::unordered_map<int, long>& bookMapper,
+                 AdjacencyMatrix* matrix, const std::string& file) {
 
     std::ifstream in(file, std::ifstream::in);
 
@@ -184,18 +154,35 @@ void readReviews(std::unordered_map<int, std::vector<int>>& reviews,
 
             std::istringstream iss(line);
 
-            std::string userID;
-            getline(iss, userID, ',');
+            std::string tempUser;
+            getline(iss, tempUser, ',');
+            int userID = std::stoi(tempUser);
 
-            std::string bookID;
-            getline(iss, bookID, ',');
+            std::string tempBook;
+            getline(iss, tempBook, ',');
+            int bookID = std::stoi(tempBook);
 
             std::string rating;
             getline(iss, rating);
 
+            // Don't process reviews on books with no isbn
+            if (bookMapper.count(bookID) != 1)
+                continue;
+
             // Filtering out ratings lower than 4
-            if (std::stoi(rating) > 3) {
-                reviews[std::stoi(userID)].push_back(std::stoi(bookID));
+            if (std::stoi(rating) == 5)
+                reviews[userID].push_back(bookID);
+            else
+                continue;
+
+            // Insert into adj. matrix
+            // Don't insert if there is only one book in the vector
+            int vectSize = reviews[userID].size();
+            if (vectSize == 1)
+                continue;
+
+            for (int i = 0; i < vectSize - 1; i++) {
+                matrix->InsertEdge(reviews[userID].at(vectSize - 1), reviews[userID].at(i));
             }
         }
 
